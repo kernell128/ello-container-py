15b0a26b50b94c3230bb8a40e450b56ac99a2fb1
Marcos Belarmino <marcosbelarmino@netscape.net>
2022-01-14 20:11:16 -0300

    my-awesome-feature

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "wello crom Sao Caetano do SUL. Blah"
+    return "Hello from a Github pull request!!!!!!”"
 
 
 if __name__ == "__main__":
