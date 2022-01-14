c2d537aa63903646f97b2be6f63570d45fd44253
Marcos Belarmino <marcosbelarmino@netscape.net>
2022-01-14 20:00:36 -0300

    Update app.py
    bla

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World, from inside the original application!"
+    return "wello crom Sao Caetano do SUL. Blah"
 
 
 if __name__ == "__main__":
